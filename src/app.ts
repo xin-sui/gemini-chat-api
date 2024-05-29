2722f8244a4cf49990e127c9b0c06fcda1e1cba9
xin-sui <108014189+xin-sui@users.noreply.github.com>
2024-05-29 19:05:44 +0800

    初始化项目

---
+++ src/app.ts
@@ -0,0 +1,67 @@
+import express from "express";
+import morgan from "morgan";
+// 限制请求频率
+import ratelimit from "express-rate-limit";
+import mongoSanitize from "express-mongo-sanitize";
+import xss from "xss-clean";
+import helmet from "helmet";
+import hpp from "hpp";
+// 引入AppError类，用于处理应用错误
+// import AppError from "./utils/appError";
+// // 引入tourRoutes和userRoutes模块
+// import tourRouter from "./routes/tourRoutes";
+// import userRouter from "./routes/userRoutes";
+// import reviewRouter from "./routes/reviewRoutes";
+// // 引入全局错误处理模块
+// import globalErrorHandler from "./controllers/errorController";
+// 创建express实例
+const app = express();
+// 设置安全头部
+app.use(helmet());
+// 开发日志
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+
+// 限制请求频率
+const limiter = ratelimit({
+    max: 100, // 每个IP地址每分钟最多100个请求
+    windowMs: 60 * 60 * 1000, // 限制时间窗口为1分钟
+    message: "请求频率太高，请稍后再试" // 当请求频率超过限制时返回的错误信息
+});
+// 使用limiter中间件
+app.use("/api", limiter);
+// 使用express的json中间件
+app.use(express.json({limit: "10kb"}));
+// 清理数据 防止跨脚本攻击 去除用户输入中的特殊字符 如$
+app.use(mongoSanitize());
+// 清理数据 防止跨站脚本攻击
+app.use(xss()); //去除html文本
+// 使用express的静态文件中间件，将public文件夹作为静态文件服务器
+app.use(express.static(`${__dirname}/public`));
+// 清理数据 去除查询字符串中重复的参数
+app.use(
+    hpp({
+        whitelist: ["discount", "ratingsQuantity", "ratingsAverage ", "duration", "maxGroupSize", "difficulty", "price"] // 设置白名单，允许的参数
+    })
+); //去除查询字符串中多余的参数
+// 记录请求时间 测试中间件
+// app.use((req, res, next) => {
+//     req.requestTime = new Date().toISOString();
+//     // console.log(req.originalUrl);
+//     next();
+// });
+
+// 3) ROUTES
+// 使用tourRouter和userRouter模块的路由
+// app.use("/api/v1/tours", tourRouter);
+// app.use("/api/v1/users", userRouter);
+// app.use("/api/v1/review", reviewRouter);
+//所有http方法 处理路由不匹配的时候
+// app.all("*", (req: {originalUrl: any}, res: any, next: (arg0: any) => void) => {
+//     next(new AppError(`在服务器没有找到${req.originalUrl}`));
+// });
+console.log(process.env.NODE_ENV);
+//全局中间件错误函数
+// app.use(globalErrorHandler);
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import app from "./app";
+
+process.on("uncaughtException", (err) => {
+    console.log("uncaughtException!");
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
+dotenv.config({path: "./config.env"});
+// const DB = process.env.DATABASE_LOCAL.replace('<PORT>', 1337);
+// const DB = process.env.DATABASE_LOCAL;
+// // console.log(DB);
+// mongoose
+//     .connect(DB, {
+//         useNewUrlParser: true,
+//         useCreateIndex: true,
+//         useFindAndModify: false,
+//         useUnifiedTopology: true
+//         // useUnifiedTopology: true
+//     })
+//     .then((con) => {
+//         // console.log(con.connections);
+//         console.log("MongoDb success");
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+});
+
+// 监听未处理的 Promise 拒绝事件
+// process.on("unhandledRejection", (err) => {
+//     // 打印错误的名称和消息
+//     console.log(err.name, err.message);
+//     server.close(() => {
+//         // 退出 Node.js 进程，1 表示非正常退出
+//         process.exit(1);
+//     });
+// });
